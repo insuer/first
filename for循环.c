1ccb99ad9ae101eb635f291bde428445f68abc88
insuer <to_git@foxmail.com>
2021-12-16 16:41:58 +0800

    as

--- for循环.c
+++ for循环.c
@@ -6,5 +6,10 @@ int main()
 	
 	printf("hello world!\n");
 	printf("hello world 2");
+<<<<<<< Updated upstream
+=======
+	printf("hello world 3");
+	printf("hello world 4");
+>>>>>>> Stashed changes
 	return 0;
 }
